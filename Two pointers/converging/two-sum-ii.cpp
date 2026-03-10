a7cd1ca1d20af65a252959f18d1012da52311118
Pratheesha B <pratheeshabenedict@gmail.com>
2026-03-10 22:35:32 +0530

    Create two-sum-ii.cpp

---
+++ Two pointers/converging/two-sum-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int i=0;
+        int j=numbers.size()-1;
+        int sum=0;
+        while(i<j){
+            int sum = numbers[i] + numbers[j];
+            if(sum>target) j--;
+            else if(sum<target) i++;
+            else{
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                break;
+            }
+        }
+        return ans;
+    }
+};
